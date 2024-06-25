321c34c415d886af75d5c7926560f7eeba1f189b
Tiger Sachse <tgsachse@gatech.edu>
2024-06-24 18:51:41 -0700

    Create skeleton app.

---
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.seclass.jobcompare6300;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle bundle) {
+    super.onCreate(bundle);
+    setContentView(R.layout.activity_main);
+  }
+}
---
+++ GroupProject/JobCompare6300/app/src/test/java/edu/gatech/seclass/jobcompare6300/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package edu.gatech.seclass.jobcompare6300;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleUnitTest {
+  @Test
+  public void addition_isCorrect() {
+    Assert.assertEquals(4, 2 + 2);
+  }
+}
