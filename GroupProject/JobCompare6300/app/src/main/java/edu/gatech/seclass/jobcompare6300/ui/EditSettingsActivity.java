ba1b355b241e1570a63465f3906fbef425bbe453
mzhao341 <mzhao341@gatech.edu>
2024-06-29 00:08:16 -0700

    drafted comparison setting UI, added seekbar

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/ui/EditSettingsActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/ui/EditSettingsActivity.java
@@ -1,21 +1,33 @@
 package edu.gatech.seclass.jobcompare6300.ui;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import edu.gatech.seclass.jobcompare6300.R;
 
 public class EditSettingsActivity extends AppCompatActivity {
+  private SeekBar seekBarYearlySalary;
+  private TextView labelYearlySalary;
 
   @Override
   protected void onCreate(Bundle bundle) {
     super.onCreate(bundle);
     setContentView(R.layout.activity_edit_settings);
 
-    var saveButton = findViewById(R.id.editSettingsSaveButton);
-    saveButton.setOnClickListener(view -> save());
+    seekBarYearlySalary = findViewById(R.id.seekBarYearlySalary);
+    labelYearlySalary = findViewById(R.id.labelYearlySalary);
 
-    var cancelButton = findViewById(R.id.editSettingsCancelButton);
-    cancelButton.setOnClickListener(view -> cancel());
+    setupSeekBarListener(seekBarYearlySalary, labelYearlySalary);
+
+    // Uncommented save and cancel button setup
+        /*
+        var saveButton = findViewById(R.id.editSettingsSaveButton);
+        saveButton.setOnClickListener(view -> save());
+
+        var cancelButton = findViewById(R.id.editSettingsCancelButton);
+        cancelButton.setOnClickListener(view -> cancel());
+        */
   }
 
   private void save() {
@@ -24,5 +36,24 @@ public class EditSettingsActivity extends AppCompatActivity {
 
   private void cancel() {
     finish();
+  }
+
+  private void setupSeekBarListener(SeekBar seekBar, TextView label) {
+    seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        label.setText(String.valueOf(progress));
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+        // Not needed for implementation
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+        // Not needed for implementation
+      }
+    });
   }
 }
